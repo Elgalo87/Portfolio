1a633641872322a5ee069d02e4b5641a27446994
Henry Barefoot <HBarefoot@users.noreply.github.com>
2017-02-21 13:03:20 -0500

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,4 +1,13 @@
 //Form validation
 $(() => {
     $('form').validator();
+    
+    //validation
+        // $("button[type='submit']").on('click', function(){
+    //   $("#name, #lastName, #email").each(function(){
+    //     if($.trim($(this).val()) == ""){
+    //       console.log("test");
+    //     }
+    //   });
+    // });
 });
